6eda4a5a29f9db91f6d0dfc2d7f8064f6668828e
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-05-09 22:02:25 +0200

    improvement: Export `.ast` module members in global module

--- src/hcl2_ast/__init__.py
+++ src/hcl2_ast/__init__.py
@@ -1,5 +1,38 @@
 __version__ = "0.1.3"
 
 from .api import parse_file, parse_string
+from .ast import (
+    Array,
+    Attribute,
+    BinaryOp,
+    Block,
+    Expression,
+    FunctionCall,
+    GetAttr,
+    Identifier,
+    Literal,
+    Module,
+    Node,
+    Object,
+    Stmt,
+    UnaryOp,
+)
 
-__all__ = ["parse_file", "parse_string"]
+__all__ = [
+    "parse_file",
+    "parse_string",
+    "Array",
+    "Attribute",
+    "BinaryOp",
+    "Block",
+    "Expression",
+    "FunctionCall",
+    "GetAttr",
+    "Identifier",
+    "Literal",
+    "Module",
+    "Node",
+    "Object",
+    "Stmt",
+    "UnaryOp",
+]
